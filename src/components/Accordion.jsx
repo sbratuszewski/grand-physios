b3fc0d4f85530b2acc2b57f1650dc11e654d0991
Bogdan <brat.bogdan.stan@gmail.com>
2025-11-04 22:38:37 +0100

    acordeon component styling and logic

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
+import { Plus, Minus } from 'lucide-react'
 
-const Accordion = () => {
-  const [accordionOpen, setAccordionpen] = React.useState(false)
+const Accordion = ({ question, answer }) => {
+  const [accordionOpen, setAccordionOpen] = React.useState(false)
   return (
     <div className="bg-gray-200 p-5 rounded-lg">
-      <button>
-        <span className="text-2xl font-semibold mb-2">
-          What services do you offer?
-        </span>
-        <span>+</span>
-      </button>
-      <p
-        className={`grid overflow-hidden transition-all duration-300 eas-in-out text-slate-600 ${accordionOpen ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0'}`}
+      <button
+        className="flex justify-between w-full items-center cursor-pointer"
+        onClick={() => setAccordionOpen(!accordionOpen)}
       >
-        We offer a range of physiotherapy services including home visits, pain
-        management, post-surgical rehabilitation, and sports injury
-        rehabilitation.
-      </p>
+        <h2 className="text-2xl font-semibold mb-2">{question}</h2>
+        {accordionOpen ? <Minus size={30} /> : <Plus size={30} />}
+      </button>
+      <div
+        className={`grid overflow-hidden transition-all duration-300 eas-in-out text-gray-800 ${accordionOpen ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0'}`}
+      >
+        <p className="overflow-hidden">{answer}</p>
+      </div>
     </div>
   )
 }
