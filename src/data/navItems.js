f6fd0a5060721e860f994ddd8042d94d7563a851
Bogdan <brat.bogdan.stan@gmail.com>
2025-11-04 10:38:27 +0100

    data file created to comply with DRY

---
+++ src/data/navItems.js
@@ -0,0 +1,9 @@
+const navItems = [
+  { name: 'Home', path: '/' },
+  { name: 'About', path: '/about' },
+  { name: 'Services', path: '/services' },
+  { name: 'Contact', path: '/contact' },
+  { name: 'FAQ', path: '/faq' },
+]
+
+export default navItems
