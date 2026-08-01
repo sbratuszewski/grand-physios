aa2169568ca6d9c70855045579adb579cb153eef
Bogdan <brat.bogdan.stan@gmail.com>
2026-08-01 19:55:34 +0200

    home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,3 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import {
-  Phone,
-  ShoppingBag,
-  ShieldCheck,
-  Award,
-  HeartHandshake,
-  Package,
-} from 'lucide-react'
-
-import heroImg from '../assets/grand-physios-hero-pic2.png'
-import Button from '../components/ui/Button'
 import HeroSection from '../components/home/HeroSection'
 import BenefitsSection from '../components/home/BenefitsSection'
 import FeaturedProducts from '../components/home/FeaturedProducts'
@@ -18,22 +5,15 @@ import ConsultationSection from '../components/home/ConsultationSection'
 import AboutSection from '../components/home/AboutSection'
 import TestimonialsSection from '../components/home/TestimonialsSection'
 
-const HomePage = () => {
-  return (
-    <>
-      <HeroSection />
-
-      <BenefitsSection />
-
-      <FeaturedProducts />
-
-      <ConsultationSection />
-
-      <AboutSection />
-
-      <TestimonialsSection />
-    </>
-  )
-}
+const HomePage = () => (
+  <>
+    <HeroSection />
+    <BenefitsSection />
+    <FeaturedProducts />
+    <ConsultationSection />
+    <AboutSection />
+    <TestimonialsSection />
+  </>
+)
 
 export default HomePage
